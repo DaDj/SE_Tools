d260ee6fdfa6b91631ea142788a1bb04627560c8
Patrick Gramann <patgramann@gmail.com>
2022-08-26 13:56:55 +0200

    envmaps, subparts updates

---
+++ Vrage_Tools/MWMBuilder/test.bat
@@ -0,0 +1,1 @@
+MwmBuilder.exe /s:"F:\Modding\VRage\VRageToolbox\Temp\Expo\Content\Models" /x:"E:\Steam\SteamApps\common\SpaceEngineersModSDK\OriginalContent\Materials" /o:"C:\Users\patgr\Desktop\Neuer Ordner" /f /e /gss
